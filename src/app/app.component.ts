ae6103419ff955e6c22cb06a04850210329728c1
SinanFischer <sinanfischer@gmail.com>
2023-01-22 13:59:28 +0100

    added animated images and started to work on slide show

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+  images = ['img_1.jpg', 'img_2.jpg', 'img_3.jpg',]; 
+  currentImage = 0; 
+  showImage = true; 
+
+  ngOnInit() {
+    this.updateImage(); 
+  }
+
+
+  updateImage() {
+
+    setInterval(() => {
+      this.currentImage++; 
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false; 
+      
+      setTimeout(() => {
+        this.showImage = true; 
+      },10); 
+    }, 8000); 
+  }
 }
